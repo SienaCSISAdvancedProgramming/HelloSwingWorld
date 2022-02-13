faa92f86bdfcc473f5e5734822e29e8035d75b56
jteresco <terescoj@gmail.com>
2022-02-13 15:16:37 -0500

    Fix indenting

--- HelloSwingWorld.java
+++ HelloSwingWorld.java
@@ -2,55 +2,53 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /**
-   A "Hello, World" program for Java Swing run as a Java application.
-
-   Based on a very similar example by Darren Lim.
-
-   @author Jim Teresco
-   @version Spring 2020
-*/
+ * A "Hello, World" program for Java Swing run as a Java application.
+ * 
+ * Based on a very similar example by Darren Lim.
+ * 
+ * @author Jim Teresco
+ * @version Spring 2022
+ */
 
 public class HelloSwingWorld {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-	// The main method is responsible for creating a thread (more
-	// about those later) that will construct and show the graphical
-	// user interface.
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		// The main method is responsible for creating a thread (more
+		// about those later) that will construct and show the graphical
+		// user interface.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 
-		/**
-		   The run method that will be invoked by this thread.
-		*/
-		@Override
-		public void run() {
+			/**
+			 * The run method that will be invoked by this thread.
+			 */
+			@Override
+			public void run() {
 
-		    // in more complex examples, you will often just
-		    // have this call a separate method
-		    
-		    // set up the GUI "look and feel" which should match
-		    // the OS on which we are running
-		    JFrame.setDefaultLookAndFeelDecorated(true);
+				// in more complex examples, you will often just
+				// have this call a separate method
 
-		    // create a JFrame in which we will build our very
-		    // tiny GUI, and give the window a name
-		    JFrame frame = new JFrame("HelloSwingWorld");
+				// set up the GUI "look and feel" which should match
+				// the OS on which we are running
+				JFrame.setDefaultLookAndFeelDecorated(true);
 
-		    // tell the JFrame that when someone closes the
-		    // window, the application should terminate
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				// create a JFrame in which we will build our very
+				// tiny GUI, and give the window a name
+				JFrame frame = new JFrame("HelloSwingWorld");
 
-		    // create a JLabel containing our message, and add
-		    // it to our JFrame
-		    JLabel label = new JLabel("Hello, Swing World!");
-		    frame.add(label);
+				// tell the JFrame that when someone closes the
+				// window, the application should terminate
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		    // display the window we've created
-		    frame.pack();
-		    frame.setVisible(true);
-		}
-	    }
-	    );
-    }
+				// create a JLabel containing our message, and add
+				// it to our JFrame
+				JLabel label = new JLabel("Hello, Swing World!");
+				frame.add(label);
+
+				// display the window we've created
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
+	}
 }
-   
